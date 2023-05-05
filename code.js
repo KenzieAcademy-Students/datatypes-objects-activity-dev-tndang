24bdb2013a6083dc73813695a6f22671f634ad5e
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-05-05 03:42:17 +0000

    deploy: 9504a6c9df628e56a4e47a90e1ee90b073573c87

--- code.js
+++ code.js
@@ -25,8 +25,45 @@ to the instructions on my.kenzie
 
 const renderPerson = function(person) {
   // Your Code Here
+  let divElement = document.createElement("div");
+  let h2Element = document.createElement("h2");
+  let h4Element1 = document.createElement("h4");
+  let h4Element2 = document.createElement("h4");
+  let h4Element3 = document.createElement("h4");
+  let ulElement = document.createElement("ul");
+  let pElement1 = document.createElement("p");
+  let pElement2 = document.createElement("p");
+  let firstName = person.firstName;
+  let lastName = person.lastName;
+  let hobbies = person.hobbies;
+  let address = person.address;
+  let favoriteColor = person["favorite color"];
 
+  document.body.append(divElement);
 
+  divElement.append(h2Element);
+  divElement.className = "card";
+  h2Element.innerText = `${firstName} ${lastName}`;
+
+  divElement.append(h4Element1);
+  h4Element1.innerText = `Hobbies`;
+  divElement.append(ulElement);
+  for (let index = 0; index < hobbies.length; index++) {
+    let liElement = document.createElement("li");
+
+    ulElement.append(liElement);
+    liElement.innerText = `${hobbies[index]}`;
+  }
+
+  divElement.append(h4Element2);
+  h4Element2.innerText = `Shipping Address`;
+  divElement.append(pElement1);
+  pElement1.innerText = `${firstName} ${lastName} \n ${address.number} ${address.street} \n ${address.city}, ${address.state} ${address.zipcode}`;
+
+  divElement.append(h4Element3);
+  h4Element3.innerText = `Favorite Color`;
+  divElement.append(pElement2);
+  pElement2.innerText = `${favoriteColor}`
 }
 
 renderPerson(examplePerson);
@@ -45,9 +82,21 @@ render your person to the page.
 */
 
 // Your Code Here
+let myExamplePerson = {
+  firstName: "Rimuru",
+  lastName: "Tempest",
+  hobbies: ["Relaxing on Shion's Lap", "Drinking Tea", "Exploring"],
+  address: {
+    number: 1,
+    street: "Rimuru's House",
+    city: "Jura",
+    state: "Tempest Federation",
+    zipcode: "15000",
+  },
+  "favorite color": "Blue",
+};
 
-
-
+renderPerson(myExamplePerson);
 
 /*
 ---------------------------------------------------------------------------
@@ -59,13 +108,19 @@ Follow the instruction on my.kenzie
 
 const changePersonToBart = function(person) {
   // Your Code Here
+  person.firstName = `Bart`
+  person.hobbies.pop();
+  person.hobbies.push(`Skateboarding`);
+  person[`favorite color`] = `orange`;
 
-
+  return person;
 }
 
 // Then, call the function with the argument examplePerson
 // Assign the result to a new variable.  Then call renderPerson with the 
 // new variable
+let examplePerson2 = changePersonToBart(examplePerson);
+renderPerson(examplePerson2);
 
 /*
 ---------------------------------------------------------------------------
